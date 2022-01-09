6ea12f7840a55d87ee1a544adf17b26bdda61fdc
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 20:59:03 +0300

    added model class for profile

--- watch/models.py
+++ watch/models.py
@@ -67,6 +67,18 @@ class Authorities(models.Model):
     def __str__(self):
         return self.name
     
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/', blank = True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = HTMLField()
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    
 class Business(models.Model):
     logo = models.ImageField(upload_to='businesslogo/')
     description = HTMLField()
@@ -84,3 +96,4 @@ class Comment(models.Model):
     comment = models.CharField(max_length=300)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    
