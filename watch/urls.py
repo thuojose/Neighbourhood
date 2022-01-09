074ebaff5ffefa86866c1633e0f986b6a1cf2dac
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 19:20:26 +0300

    created form.py

---
+++ watch/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,include
+from django.conf.urls import url
+
+
+app_name = "watch"   
+
+
+urlpatterns = [
+    path('', views.index, name = 'Index'),
+    path("register", views.register, name="register"),
+    
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
