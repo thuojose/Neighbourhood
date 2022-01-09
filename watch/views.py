f2ffdae74da7dd489ee2f1a6581af1af728fbdfb
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 21:32:43 +0300

    added function for search result

--- watch/views.py
+++ watch/views.py
@@ -207,4 +207,17 @@ def new_notification(request):
 
     return render(request, 'notifications_form.html', {"form":form})
 
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'blog' in request.GET and request.GET["blog"]:
+        search_term = request.GET.get("blog")
+        searched_blogposts = BlogPost.search_blogpost(search_term)
+        message = f"{search_term}"
 
+        print(searched_blogposts)
+
+        return render(request, 'search.html', {"message":message, "blogs":searched_blogposts})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
