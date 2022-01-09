769c9c06b30f1d83d577046eecbb4591a066a732
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 19:25:32 +0300

    added function for register

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def register(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect('/accounts/login/')
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="django_registration/registration_form.html", context={"register_form":form})
