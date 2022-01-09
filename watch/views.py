31b3aeb4bfebb0ecc81750bf9ba70c8327d1c447
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 19:52:54 +0300

    added function for notifications

--- watch/views.py
+++ watch/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
+from .models import notifications
+from .forms import NewUserForm,notificationsForm
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
@@ -17,3 +22,23 @@ def register(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="django_registration/registration_form.html", context={"register_form":form})
+
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile = Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login/')
+def notification(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+    all_notifications = notifications.objects.filter(neighbourhood=profile.neighbourhood)
+
+    return render(request, 'notifications.html', {"notifications":all_notifications})
+
