03df7aed96cfe17c6d0b4eb84fce6877667bc3d5
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 21:58:59 +0300

    created admin.py

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Neighbourhood, healthservices, notifications, Business, Health, Authorities, BlogPost, Profile
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal = ['healthservices']
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(notifications)
+admin.site.register(Health, HealthAdmin)
+admin.site.register(Business)
+admin.site.register(healthservices)
+admin.site.register(Authorities)
+admin.site.register(BlogPost)
+admin.site.register(Profile)
