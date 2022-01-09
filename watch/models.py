f7c9b6ddb8fbaa2c38e72a0a07c83840fa93b8f5
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 19:59:35 +0300

    created model class for neighbourhood

--- watch/models.py
+++ watch/models.py
@@ -8,6 +8,29 @@ Priority=(
     ('High Priority', 'High Priority'),
 )
 # Create your models here.
+# Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length = 60)
+
+    def __str__(self):
+        return self.neighbourhood_name
+
+    def create_neighbourhood(self):
+        self.save()
+        
+    @classmethod
+    def delete_neighbourhood(cls, neighbourhood_name):
+        cls.objects.filter(neighbourhood_name=neighbourhood_name).delete()
+
+    @classmethod
+    def find_neighbourhood(cls, search_term):
+        search_results = cls.objects.filter(neighbourhood_name__icontains = search_term)
+        return search_results
+
+    def update_neighbourhood(self, neighbourhood_name):
+        self.neighbourhood_name = neighbourhood_name
+        self.save()
+        
 class notifications(models.Model):
     title = models.CharField(max_length=100)
     notification = HTMLField()
