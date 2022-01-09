31d4edbc5d9568bb111b585641e04106990f3323
KHALYZ <joseph.thuo@student.moringaschool.com>
2022-01-09 20:50:16 +0300

    added form for comment

--- watch/forms.py
+++ watch/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import notifications
+from .models import notifications, Business,BlogPost, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -32,3 +32,8 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ['owner', 'neighbourhood']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['username', 'post']
